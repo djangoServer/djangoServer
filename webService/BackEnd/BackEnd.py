f88d08d8f4f7b47117208d52cf3de93dfc4ff668
Sinsanghoon <moondavil@naver.com>
2016-11-25 21:16:37 +0900

    Mileage
    many Users Connection and admin to send Mileage

--- webService/BackEnd/BackEnd.py
+++ webService/BackEnd/BackEnd.py
@@ -5,68 +5,55 @@ import threading,time
 
 sumTargetA = 0
 sumTargetB = 0
-AddMileage = 0 #추가 마일리지
+AddMileage = 0
 sumResult = 0
-Mileage = 0 #토탈 마일리지
-check = 0
+Mileage={}
+MileageArray=[]
+check = ""
 userLoginDictionary={}
-ManageId={} #마일리지를 보내주는 아이디
+Manage=""
 
 def SumMileage(request):
     global check
     global Mileage
     global AddMileage
-    global ManageId
-    check = 1
-    ManageId = request.GET.get('id', 'N/A')
+    global Manage
+    newManage = request.GET.get('manage', 'N/A')
+    newUserId = request.GET.get('id','N/A')
+    check = newUserId
     AddMileage = int(request.GET.get('AddMileage', '0'))
-    Mileage += AddMileage
-    return HttpResponse("StoreCode: " + str(ManageId) + "<br>" + "AddMileage: "+ str(AddMileage) +"<br>" + "Mileage: " + str(Mileage) + "<br><br>")
+    Mileage[newUserId][2] = Mileage[newUserId][2] + AddMileage
+    Manage=newManage
+    return HttpResponse("StoreCode: " + str(newManage) + "<br>" + str(newUserId) + "<br>" + "AddMileage: "+ str(AddMileage) +"<br>" + "Mileage: " + str(Mileage[newUserId][2]) + "<br><br>")
 
 def StreamFuncUser(myUserId):
     global check
     global Mileage
-    global ManageId
 
-    yield "Hello " + str(myUserId) + "<br>"
-    yield "Your Now Mileage:" + str(Mileage) + "<br><br>"
+    yield "Hello<br>"
 
     while True:
-        if check == 1:
-            check = 0
-            yield "StoreCode: " + str(ManageId) +" from saves Mileage<br>"
+        if check == myUserId:
+            check = ""
+            yield "StoreCode: " + str(Manage) + " from saves Mileage<br>"
             yield "AddMileage: " + str(AddMileage) + "<br>"
-            yield "Mileage: " + str(Mileage) + "<br><br>"
+            yield "Name: " + str(Mileage[myUserId][0]) + "<br>"
+            yield "Phone: " + str(Mileage[myUserId][1]) + "<br>"
+            yield "Mileage: " + str(Mileage[myUserId][2]) + "<br><br>"
         time.sleep(0.5)
-        if Mileage >= 1500:
-            yield "Mileage into change to Coupon<br>"
-            Mileage -= 1500
-            yield "Mileage : " + str(Mileage) + "<br><br>"
-
-        time.sleep(0.1)
 
     yield "Good Bye<br>"
 
-def StreamFuncManage(myUserId):
-    global userLoginDictionary
-    global Mileage
-    global check
-
-    yield "StoreCode: " + str(myUserId) + "<br><br>"
-
-    while True:
-        if check == 1:
-            yield "UserName: " + str()
-
-        time.sleep(0.1)
-
 def UserConnectionSplitTestFunc(request):
     global userLoginDictionary
-    newUserLv = int(request.GET.get('lv', '0'))
+    global Mileage
+    global Manage
     newUserId = request.GET.get('id', 'N/A')
-    if newUserLv == 1:
-        if newUserId != 'N/A' :
-            userLoginDictionary[newUserId] = "ok"
-        else:
-            return HttpResponse("fail")
-        return StreamingHttpResponse(StreamFuncUser(newUserId), content_type='text/html')
+    newUserName = request.GET.get('Name', 'N/A')
+    newUserPhone = request.GET.get('Phone', 'N/A')
+    if newUserId != 'N/A' :
+        Mileage[newUserId] = [newUserName , newUserPhone , 0 ]
+        userLoginDictionary[newUserId] = "ok"
+    else:
+        return HttpResponse("fail")
+    return StreamingHttpResponse(StreamFuncUser(newUserId))
