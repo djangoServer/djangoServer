63a6da49e9f873862d89888e2561795373f50951
stories2 <stories2@naver.com>
2016-11-30 04:14:57 +0900

    코드 최적화

--- webService/BackEnd/UserManager.py
+++ webService/BackEnd/UserManager.py
@@ -12,22 +12,32 @@ import time,MileageManager
 # 2. 마일리지 변동 여부
 # 3. 이벤트 변동 여부
 userInfoData = {}
+#여기도 숫자를 변수로 변경해 놓았는데 3번째는 아직 구현중인 것 같아서 냅두었어
+#그리고 링크에서 받아들이는 변수의 첫번째 글자는 소문자야
+
+zero = 0
+somethingEventUpdated = 1
+customersMileageStatusSavePoint = 1
+customersMileageChangedEventPoint = 2
 
 def UserConnectionStreaming(myUserID) :
+    global zero
+    global somethingEventUpdated
     while True:
-        if userInfoData[myUserID][3] == 1 :
-            userInfoData[myUserID][3] = 0
+        if userInfoData[myUserID][3] == somethingEventUpdated :
+            userInfoData[myUserID][3] = zero
             #이벤트 발생 요인 처리 과정
             yield "1"
         time.sleep(0.1)
 
 def AddUserToLogin(request) :
+    global zero
     newUserId = request.GET.get('id' , 'N/A')
-    newUserName = request.GET.get('Name', 'N/A')
+    newUserName = request.GET.get('name', 'N/A')
     if (newUserId == 'N/A'):
-        return HttpResponse("Fail")
+        return HttpResponse("fail")
     else:
-        userInfoData[newUserId] = [newUserName, 0, 0, 0]
+        userInfoData[newUserId] = [newUserName, zero, zero, zero]
     return StreamingHttpResponse(UserConnectionStreaming(newUserId))
 
 def DeleteUserToLogout(userID,userPhoneNumber) :
