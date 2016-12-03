3a98abdb205a4dbf581995a2e4e8ca195e1fac58
stories2 <stories2@naver.com>
2016-12-03 22:05:12 +0900

    외부 데이터베이스 연동

--- webService/BackEnd/DatabaseManager.py
+++ webService/BackEnd/DatabaseManager.py
@@ -1,2 +1,25 @@
 # -*- coding: utf-8 -*-
 # DB와 연동하는 소스
+
+from django.http import HttpResponse
+
+import pymysql
+
+def ConnectToDatabase():
+    return pymysql.connect(host = "lamb.kangnam.ac.kr", user = "stories2", password = "toortoor%^%", db = "AndroidTestDB", charset = "utf8")
+
+def DisconnectDatabase(databaseConnection):
+    databaseConnection.close()
+
+def ExecuteQueryToDatabase(executeAbleQuery):
+    databaseConnection = ConnectToDatabase();
+    databaseResultDataCursor = databaseConnection.cursor();
+    databaseResultDataCursor.execute(executeAbleQuery);
+    databaseResultDataRows = databaseResultDataCursor.fetchall();
+    DisconnectDatabase(databaseConnection);
+    return databaseResultDataRows;
+
+def ClientRequestQuery(request):
+    dbQuery = request.GET.get('query',';');
+    print dbQuery;
+    return HttpResponse(ExecuteQueryToDatabase(dbQuery));
