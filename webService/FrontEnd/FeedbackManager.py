a91883e01a677b6f95b10c00a5b0eef3377793b8
stories2 <stories2@naver.com>
2016-12-09 13:02:19 +0900

    메일링 서비스
    사용자 문의 내용을 메일을 이용해 받을 수 있다

--- webService/FrontEnd/FeedbackManager.py
+++ webService/FrontEnd/FeedbackManager.py
@@ -8,22 +8,31 @@ from email.mime.text import MIMEText
 
 def MailToAdmin(request):
 
+    adminEmailHost = "gmail.com"
+    adminEmailId = "stories282"
+    adminEmailPassword = "go369369"
+
     customerEmailAddress = request.GET.get('email', 'N/A')
     customerFeedbackSubject = request.GET.get('subject', 'N/A')
     customerFeedbackMessage = request.GET.get('message', 'N/A')
 
-    print customerEmailAddress
-    print customerFeedbackSubject
-    print customerFeedbackMessage
+    #print customerEmailAddress
+    #print customerFeedbackSubject
+    #print customerFeedbackMessage
+
+    """
+    설정되어있는 관리자 계정의 유저에게 메일을 보냄
+    현제 서버에는 smtp기능을 하는 포트가 제한이 있어 구글의 smtp서비스를 대신 이용함
+    """
 
     emailMessage = {}
     emailMessage['Subject'] = customerFeedbackSubject
-    emailMessage['To'] = "stories282@gmail.com"
+    emailMessage['To'] = adminEmailId + "@" + adminEmailHost
     emailMessage['From'] = customerEmailAddress
     emailMessage['Body'] = customerFeedbackMessage
 
-    smtpMessageSend = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtpMessageSend.login("stories282", "go369369")
+    smtpMessageSend = smtplib.SMTP_SSL('smtp.' + adminEmailHost, 465)
+    smtpMessageSend.login(adminEmailId, adminEmailPassword)
     smtpMessageSend.sendmail(emailMessage['From'], emailMessage['To'], str(emailMessage['Subject'] + '\n' + emailMessage['Body']))
     smtpMessageSend.quit()
 
