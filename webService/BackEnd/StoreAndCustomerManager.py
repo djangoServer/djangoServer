8314d6bfc89bf8f37031d95faa83db94da1c4bb0
Sinsanghoon <moondavil@naver.com>
2016-12-26 21:21:22 +0900

    쿠폰 정보,쿠폰 로그,판매량,최적재고량
    DB의 Table과 관계해서 Insert,Update 와 url을 작성
    또한 일부 DB직통 url을 함수를 거쳐서 진입(단순 우회)

--- webService/BackEnd/StoreAndCustomerManager.py
+++ webService/BackEnd/StoreAndCustomerManager.py
@@ -1,16 +1,32 @@
 # -*- coding: utf-8 -*-
 # 사용자와 매장과의 관계에 관한 사항을 다루는 소스
 from django.http import HttpResponse
+import DatabaseManager
 
 def AddNewCustomerToTargetStore(request) :
+    """
     myUserId = request.GET.get('userId', 'N/A')
     myStoreId = request.GET.get('storeId', 'N/A')
 
     dbQuery = "INSERT INTO 매장등록 정보 VALUES( " + str(myUserId) + str(myStoreId) + "," + str(myUserId) + ", " + str(myStoreId) + " , false);"
-    return HttpResponse("OK")
+    """
+
+    return HttpResponse(DatabaseManager.AddToStoreAsNewMember(request))
 
 def BanCustomerFromTargetStore(request) :
+    """
     myUniqueId = request.GET.get('uniqueId', 'N/A')
 
     dbQuery = "UPDATE 매장등록 정보 SET 회원탈퇴여부 = true WHERE 고유등록번호 = "+ str(myUniqueId)  +";"
-    return HttpResponse("OK")
+    """
+
+    return HttpResponse(DatabaseManager.DelMemberFromStore(request))
+
+def GetStoreAndCustomerRegisteredId(request) :
+    return HttpResponse(DatabaseManager.GetStoreAndCustomerRegisteredId(request))
+
+def SendNotificationMessage(request) :
+    return HttpResponse(DatabaseManager.SendNotificationMessage(request))
+
+def SendCoupon(request) :
+    return HttpResponse(DatabaseManager.SendCouponMessage(request))
