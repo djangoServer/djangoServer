7020daf9e1d5b80d4f4a17dd5cfb66834e4e6906
Sinsanghoon <moondavil@naver.com>
2017-01-19 01:42:59 +0900

    파일 빼 먹음
    커밋 중 파일을 실수로 빼 먹음

--- webService/BackEnd/Database/NoticeDatabase.py
+++ webService/BackEnd/Database/NoticeDatabase.py
@@ -4,6 +4,7 @@
 
 from django.http import HttpResponse
 import pymysql
+from django.http import JsonResponse
 
 def ConnectToDatabase():
     return pymysql.connect(host = "lamb.kangnam.ac.kr", user = "serviceAdmin", password = "1029384756", db = "ServiceDatabase", charset = "utf8", autocommit=True)
@@ -27,51 +28,65 @@ def InsertNewStoreNoticeInfo(request):
 
     try:
         shopId = request.GET.get('shopId', None)
-        noticeTitle = request.GET.get('noticeTitle', '')
-        noticeBody = request.GET.get('noticeBody', '')
-        noticeStartDate = request.GET.get('noticeStartDate', '0000-00-00')
-        noticeStopDate = request.GET.get('noticeStopDate', '0000-00-00')
-        noticeLastUpdateDate = request.GET.get('noticeLastUpdateDate', '0000-00-00')
-
-        if shopId == None:
-            return HttpResponse("Fail")
-
-        databaseQuery = "insert into `매장공지 정보` (`매장번호`, `제목`, `내용`, `공지 시작 날짜`, `공지 마감 날짜`, `마지막 편집 날짜`)" \
-        + " values (" + shopId + ", '" + noticeTitle + "', '" + noticeBody + "', '" + noticeStartDate + "', '" + noticeStopDate + "', '" \
-        + noticeLastUpdateDate + "');"
-
-        queryResultData = ExecuteQueryToDatabase(databaseQuery)
-    except:
-        print "Error in InsertNewStoreNoticeInfo: " + queryResultData
-    return HttpResponse(queryResultData)
-
-#기존의 공지사항 편집
-def UpdateStoreNoticeInfo(request):
-    queryResultData = None
-    databaseQuery = None
-
-    try:
-        shopId = request.GET.get('shopId', None)
         noticeId = request.GET.get('noticeId', None)
         noticeTitle = request.GET.get('noticeTitle', '')
         noticeBody = request.GET.get('noticeBody', '')
         noticeStartDate = request.GET.get('noticeStartDate', '0000-00-00')
         noticeStopDate = request.GET.get('noticeStopDate', '0000-00-00')
-        noticeLastUpdateDate = request.GET.get('noticeLastUpdateDate', '0000-00-00')
+        noticeLastUpdateDate = request.GET.get('noticeLastUpdateDate', None)
 
-        if shopId == None and noticeId == None:
-            return HttpResponse("Fail")
+        if shopId == None or noticeId == None:
+            return JsonResponse({'Result' : 'Fail'})
 
-        databaseQuery = "update `매장공지 정보`" \
-        + " set `제목` = '" + noticeTitle + "', `내용` = '" + noticeBody + "', `공지 시작 날짜` = '" + noticeStartDate + "', " \
-        + "`공지 마감 날짜` = '" + noticeStopDate + "', `마지막 편집 날짜` = '" + noticeLastUpdateDate + "' " \
-        + "where `매장번호` = " + shopId + " and `공지번호` = " + noticeId + ";"
-
+        if noticeLastUpdateDate != None:
+            databaseQuery = "insert into `매장공지 정보` (`매장번호`, `공지번호`, `제목`, `내용`, `공지 시작 날짜`, `공지 마감 날짜`, `마지막 편집 날짜`)" \
+                            + " values (" + shopId + ", " + noticeId + ", '" + noticeTitle + "', '" + noticeBody + "', '" + noticeStartDate + "', '" + noticeStopDate + "', '" \
+                            + noticeLastUpdateDate + "');"
+        else:
+            databaseQuery = "insert into `매장공지 정보` (`매장번호`, `공지번호`, `제목`, `내용`, `공지 시작 날짜`, `공지 마감 날짜`)" \
+                            + " values (" + shopId + ", " + noticeId + ", '" + noticeTitle + "', '" + noticeBody + "', '" + noticeStartDate + "', '" + noticeStopDate + "');"
+        print databaseQuery
         queryResultData = ExecuteQueryToDatabase(databaseQuery)
-
+        return JsonResponse({'Result': 'Ok'})
     except:
-        print "Error in UpdateStoreNoticeInfo: " + queryResultData
-    return HttpResponse(queryResultData)
+        return JsonResponse({'Result': 'Fail'})
+
+#기존의 공지사항 편집
+def UpdateStoreNoticeInfo(request):
+
+    storeNoticeInfo = {}
+
+    storeNoticeInfo['매장번호'] = request.GET.get('shopId', None)
+    storeNoticeInfo['공지번호'] = request.GET.get('noticeId', None)
+    storeNoticeInfo['제목'] = request.GET.get('noticeTitle', None)
+    storeNoticeInfo['내용'] = request.GET.get('noticeBody', None)
+    storeNoticeInfo['공지 시작 날짜'] = request.GET.get('noticeStartDate', None)
+    storeNoticeInfo['공지 마감 날짜'] = request.GET.get('noticeStopDate', None)
+    storeNoticeInfo['마지막 편집 날짜'] = request.GET.get('noticeLastUpdateDate', None)
+
+    if storeNoticeInfo['매장번호'] == None and storeNoticeInfo['공지번호'] == None:
+        return JsonResponse({'Result': 'Fail'})
+
+    databaseQuery = "update `매장공지 정보` set "
+
+    multipleUpdate = False
+
+    for indexOfAvailableKey in storeNoticeInfo:
+        if storeNoticeInfo[indexOfAvailableKey] != None and indexOfAvailableKey != "매장번호" and indexOfAvailableKey != "공지번호":
+            if multipleUpdate == True:
+                databaseQuery = databaseQuery + ", "
+
+            databaseQuery = databaseQuery + "`" + indexOfAvailableKey + "` = '" + storeNoticeInfo[indexOfAvailableKey] + "'"
+            multipleUpdate = True
+
+    databaseQuery = databaseQuery + " where `매장번호` = " + storeNoticeInfo['매장번호'] + " and `공지번호` = " + storeNoticeInfo['공지번호'] + ";"
+
+    try:
+        print databaseQuery
+        queryResultData = ExecuteQueryToDatabase(databaseQuery)
+        return JsonResponse({'Result': 'Ok'})
+    except:
+        return JsonResponse({'Result': 'Fail'})
 
 #기존의 공지사항 삭제
 def DelStoreNoticeInfo(request):
@@ -81,12 +96,57 @@ def DelStoreNoticeInfo(request):
     shopId = request.GET.get('shopId', None)
     noticeId = request.GET.get('noticeId', None)
 
+    if shopId == None or noticeId == None:
+        return JsonResponse({'Result' : 'Fail'})
+
     try :
         databaseQuery = "update `매장공지 정보`"\
-                        + "set `삭제 여부` = 1" \
+                        + " set `삭제 여부` = 1 " \
                         + "where `매장번호` = " + shopId + " and `공지번호` = " + noticeId + ";"
+        print databaseQuery
         queryResultData = ExecuteQueryToDatabase(databaseQuery)
+        return JsonResponse({'Result' : 'Ok'})
     except :
-        print "Error in DelStoreNoticeInfo: " + queryResultData
+        return JsonResponse({'Result' : 'Fail'})
 
-    return HttpResponse(queryResultData)
+def ShowTargetStoreNoticeList(request):
+
+    noticeListData = {}
+    storeNoticeInfo = {}
+
+    storeNoticeInfo['매장번호'] = 0
+    storeNoticeInfo['공지번호'] = 1
+    storeNoticeInfo['제목'] = 2
+    storeNoticeInfo['내용'] = 3
+    storeNoticeInfo['공지 시작 날짜'] = 4
+    storeNoticeInfo['공지 마감 날짜'] = 5
+    storeNoticeInfo['마지막 편집 날짜'] = 6
+    storeNoticeInfo['이미지 저장 경로'] = 7
+    storeNoticeInfo['삭제 여부'] = 8
+
+    shopId = request.GET.get('shopId', None)
+
+    if shopId == None:
+        return JsonResponse({'Result' : 'Ok'})
+
+    try:
+        databaseQuery = "select * from `매장공지 정보` where `매장번호` = " + shopId + ";"
+        print databaseQuery
+        queryResultData = ExecuteQueryToDatabase(databaseQuery)
+
+        for indexOfResult in range(0, queryResultData.__len__()):
+            noticeListData[indexOfResult] = {
+                                            '매장번호' : queryResultData[indexOfResult][storeNoticeInfo['매장번호']],
+                                            '공지번호' : queryResultData[indexOfResult][storeNoticeInfo['공지번호']],
+                                            '제목' : queryResultData[indexOfResult][storeNoticeInfo['제목']],
+                                            '내용' : queryResultData[indexOfResult][storeNoticeInfo['내용']],
+                                            '공지 시작 날짜' : str(queryResultData[indexOfResult][storeNoticeInfo['공지 시작 날짜']]),
+                                            '공지 마감 날짜' : str(queryResultData[indexOfResult][storeNoticeInfo['공지 마감 날짜']]),
+                                            '마지막 편집 날짜' : str(queryResultData[indexOfResult][storeNoticeInfo['마지막 편집 날짜']]),
+                                            '이미지 저장 경로' : queryResultData[indexOfResult][storeNoticeInfo['이미지 저장 경로']],
+                                            '삭제 여부' : queryResultData[indexOfResult][storeNoticeInfo['삭제 여부']],
+                                            }
+        return JsonResponse(noticeListData)
+    except:
+        return JsonResponse({'Result' : 'Fail'})
+
